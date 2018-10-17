d2d3b65fb396800d6f2f25e4f17c32593d158279
Alexey Kryazhev <ubivaza@gmail.com>
2018-10-17 13:37:29 +0300

    add default value to Opitonal

--- src/validators/Optional.js
+++ src/validators/Optional.js
@@ -2,11 +2,11 @@ import { composeValidators } from '../utils';
 
 
 export default
-function Optional (vs) {
+function Optional (vs, defaultValue) {
   vs = composeValidators(vs);
 
   return function (d, ...args) {
-    if (d === undefined) return [null, d];
+    if (d === undefined) return [null, defaultValue];
     return vs(d, ...args);
   };
 }

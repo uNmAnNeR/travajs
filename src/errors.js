b13f63b8253421fd12879842f6cd45c8c4551480
Alexey Kryazhev <ubivaza@gmail.com>
2018-10-30 12:08:43 +0300

    stringify errors

--- src/errors.js
+++ src/errors.js
@@ -1,25 +1,34 @@
 // @flow
 
 
-import { isString } from './utils';
-
 export
 class ValidationError<T> extends Error {
   message: string;
   data: T;
-  static extractData: (Error) => string;
+  static extractData: any => string;
+  static stringify: any => string;
 
   constructor (data: T) {
-    super(JSON.stringify(data));
+    super(ValidationError.stringify(data));
     this.name = 'ValidationError';
     this.data = data;
   }
-
-  toString (): string {
-    return JSON.stringify(this.data);
-  }
 }
-ValidationError.extractData = function (error: Error): string {
+ValidationError.extractData = function (error: any): string {
   if (error instanceof ValidationError) return error.data;
   return String(error);
+}
+ValidationError.stringify = function (error: any): string {
+  if (error instanceof ValidationError) return ValidationError.stringify(error.data);
+  if (error instanceof Error) return error.message;
+
+  try {
+    return JSON.stringify(error);
+  } catch (e) {}
+
+  try {
+    return String(error);
+  } catch (e) {}
+
+  return '';
 }

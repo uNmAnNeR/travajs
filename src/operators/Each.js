dcce7752620db2d2716929f150e5d51b4c3794d5
Alexey Kryazhev <ubivaza@gmail.com>
2018-11-06 13:50:55 +0300

    add Each `errorTo` option

--- src/operators/Each.js
+++ src/operators/Each.js
@@ -4,7 +4,7 @@ import { ValidationError } from '../errors';
 // TODO flow
 // TODO Currently works only for Arrays
 export default
-function Each (validator) {
+function Each (validator, eachOpts=Each.DEFAULTS) {
   validator = Compose(validator);
 
   return function (coll, opts) {
@@ -14,8 +14,14 @@ function Each (validator) {
     for (let i=0; i<coll.length; i++) {
       const res = validator(coll[i], i, coll, opts);
       if (res instanceof Error) {
-        if (!errors) errors = {};
-        errors[i] = ValidationError.extractData(res);
+        const errorData = ValidationError.extractData(res);
+        if (eachOpts.errorsTo === Array) {
+          if (!errors) errors = [];
+          errors.push(errorData);
+        } else {
+          if (!errors) errors = {};
+          errors[i] = errorData;
+        }
       } else {
         valid.push(res);
       }
@@ -26,3 +32,6 @@ function Each (validator) {
       valid;
   }
 }
+Each.DEFAULTS = {
+  errorsTo: Object,
+};
